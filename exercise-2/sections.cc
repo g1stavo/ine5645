cdc5687f427874e10d9d0d0b4301829480d112df
gustavocsalvador <gustavocsalvador@gmail.com>
2018-04-02 21:40:27 -0300

    First efforts

---
+++ exercise-2/sections.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+
+void sect_example(){
+	#pragma omp parallel sections
+	{
+		#pragma omp section
+			std::cout << "Section 1!\n";
+
+		#pragma omp section
+			std::cout << "Section 2!\n";
+
+		#pragma omp section
+			std::cout << "Section 3!\n";
+	}
+}
+
+int main()
+{
+	sect_example();
+}
